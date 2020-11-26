cac4215fdff0eda4e8c6b4923a19f8cbaa35b2e4
benanamen <ducatikevin@yahoo.com>
2020-11-26 10:52:36 -0800

    Release v.1.1

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 require 'bootstrap.php';
 
-$path = strtolower(trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
+echo $path = strtolower(trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
 $subDirPath = 'squire-rethink';// No trailing slash
 
 $routes = [
     $subDirPath => 'default',
-    $subDirPath . 'test' => 'test',
-    $subDirPath . 'contact' => 'contact',
-    $subDirPath . 'index.php' => 'default'
+    $subDirPath . '/test' => 'test',
+    $subDirPath . '/contact' => 'contact',
+    $subDirPath . '/index.php' => 'default'
 ];
 
 view('layout', ['page' => array_key_exists($path, $routes) ? $routes[$path] : '404']);
